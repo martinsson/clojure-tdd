17a00e8f19bf064f57fbefa1884ff53bd4854d22
Johan Martinsson <jm1974@hotmail.com>
2011-07-17 00:02:18 +0200

    updated project to use midje 1.1.1

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [midje "1.1-alpha-1"]]
+                 [midje "1.1.1"]]
   :dev-dependencies [
         [lein-eclipse "1.0.0"]]
 )
