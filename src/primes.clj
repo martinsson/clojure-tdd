528a2cac1d777b9ffe9414ecea6b60589b55c895
Johan Martinsson <jm1974@hotmail.com>
2011-07-16 21:53:11 +0200

    working on problem41

--- src/primes.clj
+++ src/primes.clj
@@ -1,4 +1,8 @@
-(ns primes)
+(ns primes
+  (:use [clojure-tdd.core] :reload)
+  (:use [clojure.test])    
+  (:use [midje.sweet])
+  (:use [clojure.contrib.combinatorics]))
 
 
 (defn lazy-primes []
@@ -20,3 +24,23 @@
                 (lazy-seq (next-primes (next-sieve sieve candidate)
                             (+ candidate 2))))))]
     (cons 2 (lazy-seq (next-primes {} 3)))))
+(defn problem7 []
+  (nth (lazy-primes) 10000))
+(defn problem10 []
+  (reduce + (take-while #(< % 2000000) (lazy-primes))))
+
+(defn prime? [prime-generator n]
+  (some 
+    (partial = n) 
+    (for [prime (prime-generator) :while (<= prime n ) ] prime )))
+(fact 
+  (prime? lazy-primes 10) => falsey
+  (prime? lazy-primes 17) => truthy
+  (prime? lazy-primes 2) => truthy)
+
+(defn problem41 [n] 
+  (first (filter 
+           (partial prime? (memoize lazy-primes)) 
+           (map 
+             #(Integer. (apply str %)) 
+             (permutations (range n 0 -1) )))))
