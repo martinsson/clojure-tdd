61210d98ce4ec40efb669b0a2d8fe9dd437ccec7
Johan Martinsson <jm1974@hotmail.com>
2011-10-18 17:36:35 +0200

    a version without much of records

--- src/medics.clj
+++ src/medics.clj
@@ -3,40 +3,57 @@
   (:use [clojure.test])    
   (:use [midje.sweet]))
 
-(unfinished box-size prescription)
 
-(defn ciel-quot [quantity batch-size]
-  "lowest multiple of batch-size that is greater than or equal to quantity"
-  (inc (quot (dec quantity) batch-size)))
+(defn box-size [medic] 10)
 
-(defn no-pills [{:keys [days number]}]
-  (* days number))
+(defn ciel-quot [num div]
+  (inc (quot (dec num) div)))
 
-(fact 
-  "multiplies the number of pills per day by the number of days"
-  (no-pills {:days 7 :number 1}) => 7
-  )
-(future-fact   (no-pills {:days 7 :posology [:matin :midi] :number 1}) => 14)
-(defn order [p]
-  (ciel-quot (no-pills p) (box-size)))
+(defn nb-pills [{:keys [quantity takes-per-day days]}]
+  (* quantity takes-per-day days))
+
+(defn boxes [{:keys [posology medic]}]
+  (ciel-quot (nb-pills posology) (box-size medic)))
+
+(defprotocol PillCalc
+  (nb-pills [posology]))
+(defrecord ConstantPosology [quantity takes-per-day days]
+  PillCalc
+  (nb-pills []))
+(defrecord Prescription [medic posology])
+(defrecord OrderItem [medic boxes])
+
+(defn order [prescriptions]
+  (let [order-item (fn [{medic :medic :as prescription}]
+                     (OrderItem. medic (boxes prescription)))]
+    (map order-item prescriptions)))
+
+;;----
 
 (fact
-  "for 1 pill the order is 1 box"
-  (order ...prescription...) => 1
-  (provided
-    (box-size) => 10
-    (no-pills ...prescription...) => 1)
-  
-  "for 11 pills the order is 2 boxes of 10"
-  (order ...prescription...) => 2
-  (provided
-    (box-size) => 10
-    (no-pills ...prescription...) => 11)
+  (nb-pills {:quantity 1 :takes-per-day 1 :days 5}) => 5
+  (nb-pills {:quantity 2 :takes-per-day 1 :days 5}) => 10
+  (nb-pills {:quantity 2 :takes-per-day 3 :days 5}) => 30
+  )
+(let [prescription {:posology ...posology... :medic ...medic...}] 
+  (fact
+    "we are given the minimum amount of boxes that covers the posology"
+	  (boxes prescription) => 1
+	    (provided 
+	      (nb-pills ...posology...) => 1
+	      (box-size ...medic...) => 10)
+	  (boxes prescription) => 2
+	    (provided 
+	      (nb-pills ...posology...) => 20
+	      (box-size ...medic...) => 10)))
 
-  "for 10 pills the order is 1 boxes of 10"
-  (order ...prescription...) => 1
-  (provided
-    (box-size) => 10
-    (no-pills ...prescription...) => 10))
-
-
+(fact
+  "an order can contain several prescriptions for different medics"
+  (order [{:medic "paracetamol" :posology ...paracetamol...} {:medic "paracyl" :posology ...paracyl...}]) 
+     => [(OrderItem. "paracetamol" 2) (OrderItem. "paracyl" 3)]
+    (provided 
+      (box-size "paracetamol") => 15
+      (nb-pills ...paracetamol...) => 20
+      (box-size "paracyl") => 7
+      (nb-pills ...paracyl...) => 15
+      ))
