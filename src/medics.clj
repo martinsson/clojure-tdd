dce260ccb3e541ab60f587f2c982582783e5b141
Johan Martinsson <jm1974@hotmail.com>
2011-10-21 11:30:23 +0200

    handling varaible posology

--- src/medics.clj
+++ src/medics.clj
@@ -4,55 +4,66 @@
   (:use [midje.sweet]))
 
 
-(defn box-size [medic] 10)
+(unfinished box-size)
 
 (defn ciel-quot [num div]
   (inc (quot (dec num) div)))
 
-
-(defn boxes [{:keys [posology medic]}]
-  (ciel-quot (nb-pills posology) (box-size medic)))
-
+;; sort of a java interface
 (defprotocol PillCalc
-  (nb-pills [posology]))
+  (nb-pills
+    "The number of pills necessary for the whole duration of the cure"
+    [posology]))
 
 (defrecord ConstantPosology [quantity takes-per-day days]
-  PillCalc
+  PillCalc  ;; The data structure ConstantPosology implements the PillCalc abstraction
   (nb-pills [posology] (* quantity takes-per-day days)))
+(fact
+  (nb-pills (ConstantPosology. 1 1 5)) => 5
+  (nb-pills (ConstantPosology. 2 3 5)) => 30)
 
-(defrecord Prescription [medic posology])
+(defrecord VariablePosology [per-day days]
+  PillCalc  ;; VariablePosology implements in another way
+  (nb-pills [posology] (let [pills-per-day (apply + (vals per-day))] 
+                         (* pills-per-day days))))
+(fact 
+  (nb-pills (VariablePosology. {:morning 2} 5)) => 10
+  (nb-pills (VariablePosology. {:before-breakfast 1 :before-dinner 2} 7)) => 21)
+
+(defn boxes [[medic posology]]
+  (ciel-quot (nb-pills posology) (box-size medic)))
+
 (defrecord OrderItem [medic boxes])
 
 (defn order [prescriptions]
-  (let [order-item (fn [{medic :medic :as prescription}]
-                     (OrderItem. medic (boxes prescription)))]
-    (map order-item prescriptions)))
+  ;; lets declare a function order-item that creates an OrderItem given a prescription-entry
+  (let [order-item (fn [prescription] 
+                     (OrderItem. (key prescription) (boxes prescription)))]
+    ;; now we can project the list of prescription entries to a list of OrderItems 
+    ;; by applying order-item to each element in prescriptions
+    (map order-item prescriptions)))  
 
-;;----
-
+;;---- Facts about boxes and order
 (fact
-  (nb-pills (ConstantPosology. 1 1 5)) => 5
-  (nb-pills (ConstantPosology. 2 1 5)) => 10
-  (nb-pills (ConstantPosology. 2 3 5)) => 30)
-(let [prescription {:posology ...posology... :medic ...medic...}] 
-  (fact
     "we are given the minimum amount of boxes that covers the posology"
-	  (boxes prescription) => 1
-	    (provided 
-	      (nb-pills ...posology...) => 1
-	      (box-size ...medic...) => 10)
-	  (boxes prescription) => 2
-	    (provided 
-	      (nb-pills ...posology...) => 20
-	      (box-size ...medic...) => 10)))
+	  (let [posology [...medic... ...posology...]] 
+     (boxes posology) => 1
+		    (provided 
+		      (nb-pills ...posology...) => 1
+		      (box-size ...medic...) => 10)
+		  (boxes posology) => 2
+		    (provided 
+		      (nb-pills ...posology...) => 20
+          (box-size ...medic...) => 10)))
 
 (fact
   "an order can contain several prescriptions for different medics"
-  (order [{:medic "paracetamol" :posology ...paracetamol...} {:medic "paracyl" :posology ...paracyl...}]) 
-     => [(OrderItem. "paracetamol" 2) (OrderItem. "paracyl" 3)]
+  (order {:paracetamol ...posology-cetamol... 
+          :paracyl ...posology-paracyl...}) 
+     => [(OrderItem. :paracetamol 2) (OrderItem. :paracyl 3)]
     (provided 
-      (box-size "paracetamol") => 15
-      (nb-pills ...paracetamol...) => 20
-      (box-size "paracyl") => 7
-      (nb-pills ...paracyl...) => 15
+      (box-size :paracetamol) => 15
+      (nb-pills ...posology-cetamol...) => 20
+      (box-size :paracyl) => 7
+      (nb-pills ...posology-paracyl...) => 15
       ))
